d0b0d51197116b9f03c463cd65d0cae2829af4ae
abhishekas7 <abhishekas2023a@mca.ajce.in>
2023-12-10 23:25:13 +0530

    text abhishek added

--- my-portfolio/next.config.js
+++ my-portfolio/next.config.js
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+// next.config.js
+module.exports = {
+  webpack(config) {
+    config.infrastructureLogging = { debug: /PackFileCache/ }
+    return config;
+  }
 }
-
-module.exports = nextConfig
--- my-portfolio/src/pages/components/Banner/Banner.jsx
+++ my-portfolio/src/pages/components/Banner/Banner.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
 import style from "./Banner.module.css"
 
+
 const Banner = () => {
   return (
     <>
     
     <div className={`w-100 h-[780px] bg-color-1 relative overflow-hidden`}>
 <div className={` bg-color-3 w-[315px] h-[315px] ${style.blurcircle} absolute top-4 left-8`}>
-  
 </div>
+<div className={` bg-color-3 w-[315px] h-[315px] ${style.blurcircle} absolute top-4 right-8`}>
+</div>
+<div className={` bg-color-3 w-[315px] h-[315px] ${style.blurcircle} absolute bottom-1 right-[56rem]`}>
+</div>
+
+<div className={`bg-color-3  min-w-[600px] min-h-[600px] overflow-hidden ${style.animationvideo} absolute right-[6rem] top-[90px]`}>
+
+
+</div>
+
+<div className={` min-w-[900px] min-h-[600px] overflow-hidden  absolute left-[6rem] top-[90px]`}>
+
+<div className={`${style.nameStrock}`}>
+  I AM ABHISHEK A S
+</div>
+
+</div>
+
+
 
 
 
